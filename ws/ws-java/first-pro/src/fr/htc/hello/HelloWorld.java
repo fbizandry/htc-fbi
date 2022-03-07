9e0a54bbc0ae618e33d7afe9cc8de8bb892d2577
fbizandry <fbizandrytiavarisan@htcompass.fr>
2022-03-07 19:00:09 +0100

    07/03/2022-1

--- ws/ws-java/first-pro/src/fr/htc/hello/HelloWorld.java
+++ ws/ws-java/first-pro/src/fr/htc/hello/HelloWorld.java
@@ -1,7 +1,6 @@
 package fr.htc.hello;
 
 public class HelloWorld {
-	const int i = 5;
 	public static void main(String[] args) {
 		System.out.println("Application Addition ...");
 		if (args.length != 5) {
---
+++ ws/ws-java/java-standard/src/fr/htc/java/simple/JavaStandardTesting.java
@@ -0,0 +1,16 @@
+package fr.htc.java.simple;
+
+public class JavaStandardTesting {
+	
+	public static void main(String[] args) {
+		 
+             int a = 5;
+             int b = 150;
+
+             int som = a + b;
+
+             System.out.println("La somme est : " + som);
+     }
+	}
+
+
